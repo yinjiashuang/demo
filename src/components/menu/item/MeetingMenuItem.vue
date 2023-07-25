700b134e74aa690579fb37f2c63b0337185dd758
mashipeng <congduan970818@163.com>
2023-07-25 13:53:58 +0800

    图片问题修改

--- src/components/menu/item/MeetingMenuItem.vue
+++ src/components/menu/item/MeetingMenuItem.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="meeting-menu-item">
-    <img class="item-background" :src="imageUrl" />
-    <img
-      class="item-icon"
-      :src="`@/assets/meeting/${props.iconName}.svg`"
-      :alt="props.btnName"
-    />
-    <div class="item-text">
-      {{ props.btnName }}
+    <div class="meeting-menu-item">
+        <img class="item-background" :src="backgroundUrl" alt=""/>
+        <img
+            class="item-icon"
+            :src="iconUrl"
+            :alt="props.btnName"/>
+        <div class="item-text">
+            {{ props.btnName }}
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -20,10 +19,8 @@ const props = defineProps({
     iconName: String,
     backgroundName: String
 });
-
-const imageUrl = require(`@/assets/meeting/${props.backgroundName}.svg`);
-console.log(`----------`, imageUrl);
-// url('@renderer/assets/meeting/${props.backgroundName}.svg')
+const iconUrl = require(`@/assets/meeting/${props.iconName}.svg`)
+const backgroundUrl = require(`@/assets/meeting/${props.backgroundName}.svg`);
 </script>
 
 <style lang="less">
